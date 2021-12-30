cdcfdb5ff4695cec917d2af5ca3b2aa6f4b486c7
Brandon! <brandoge91@gmail.com>
2021-12-30 10:10:02 -0500

    Create viewsuspensions.ts

---
+++ ViewSuspensionPlugin/viewsuspensions.ts
@@ -0,0 +1,55 @@
+import { discordClient, robloxClient, robloxGroup } from '../../main';
+import { CommandContext } from '../../structures/addons/CommandAddons';
+import { Command } from '../../structures/Command';
+import {
+    getInvalidRobloxUserEmbed,
+    getRobloxUserIsNotMemberEmbed,
+    getSuccessfulUnsuspendEmbed,
+    getUnexpectedErrorEmbed,
+    getVerificationChecksFailedEmbed,
+    getRoleNotFoundEmbed,
+    getNotSuspendedEmbed,
+    getAlreadySuspendedEmbed,
+    noSuspendedRankLog,
+    mainColor,
+} from '../../handlers/locale';
+import { config } from '../../config';
+import { provider } from '../../database/router';
+import { MessageEmbed } from 'discord.js';
+
+
+class ViewSuspensionsCommand extends Command {
+    constructor() {
+        super({
+            trigger: 'viewsuspensions',
+            description: 'Allows the viewing of all concurrent suspensions.',
+            type: 'ChatInput',
+            module: 'SUSPENSIONS',
+            permissions: [
+                {
+                    type: 'role',
+                    id: config.permissions.ranking,
+                    value: true,
+                }
+            ],
+        });
+    }
+
+    async run(ctx: CommandContext) {
+        if(!config.database.enabled) return ctx.reply({ embeds: [ getUnexpectedErrorEmbed() ] });
+
+        const suspensions = await provider.findSuspendedUsers();
+        let mainEmbed = new MessageEmbed();
+        mainEmbed.setTimestamp();
+        mainEmbed.setColor(mainColor);
+        mainEmbed.setTitle('Current Suspensions');
+        for (var i in suspensions) {
+            const user = await robloxClient.getUser(suspensions[i].robloxId);
+
+            mainEmbed.addField(user.name,`Expires on ${suspensions[i].suspendedUntil.toDateString()}`);
+        }
+        return ctx.reply({embeds: [mainEmbed]});
+    }
+}
+
+export default ViewSuspensionsCommand;
